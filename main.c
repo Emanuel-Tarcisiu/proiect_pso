65f2733b96fa1399001633f9bb4bd3fa77cb723b
Emanuel-Tarcisiu <orsanemanuel@yahoo.com>
2023-10-28 17:55:20 +0300

    update test

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
-int main(){
+int main(int argc, char* argv[]){
     printf("Hello there\n\n");
+
+    if(argc>1){
+        for(int i=0;i<argc;i++)
+            printf("Arg[%i]: %s\n",i,argv[i]);
+
+        printf("\n");
+    }
+    
     return 0;
 }
