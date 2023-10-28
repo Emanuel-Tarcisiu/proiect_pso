0be9363352f8dd25777cdd80feb796c34c097ea0
Emanuel-Tarcisiu <orsanemanuel@yahoo.com>
2023-10-28 23:27:04 +0300

    makefile si script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Se ruleaza Makefile:
+    make -f Makefile 
+
+echo Se ruleaza ./main:
+echo
+    ./main
--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
+#include"unistd.h"
 
 int main(int argc, char* argv[]){
     printf("Hello there\n\n");
 
-    if(argc>1){
+    if(argc > 1){
         for(int i=0;i<argc;i++)
             printf("Arg[%i]: %s\n",i,argv[i]);
 
         printf("\n");
     }
-    
+
+    write(STDOUT_FILENO,"Sfarsit\n",8);
     return 0;
 }
