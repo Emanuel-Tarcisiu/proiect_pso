4a92a71d72aaa67c73d3852ab2eb81b0b4f8449f
Emanuel-Tarcisiu <orsanemanuel@yahoo.com>
2023-11-05 20:09:59 +0200

    test mymalloc main

--- main.c
+++ main.c
@@ -1,44 +1,63 @@
-// #include<stdio.h>
-// #include"unistd.h"
-// #include<sys/mman.h>
-
-// int main(int argc, char* argv[]){
-//     printf("Top of heap: %p\n",sbrk(0));
-
-//     int* addr=(int*)mmap(sbrk(0), sizeof(int), PROT_READ | PROT_WRITE, MAP_PRIVATE, 0, 0);
-//     addr=12;
-//     printf("nr dupa alocare: %i\n",addr);
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<stdio.h>
 
 
-//     printf("Top of heap(dupa mmap): %p\n",sbrk(0));
-//     munmap(addr,sizeof(int));
+#define SIZE_HEAP 64
 
-//     printf("Top of heap(dupa munmap): %p\n",sbrk(0));
-//     return 0;
-// }
+typedef struct data_block
+{
+    struct data_block* next;
+    struct data_block* prev;
+    size_t dim_bloc;
+    bool liber;
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/mman.h>
+}data_block;
 
-int main(int argc, char* argv[]) {
-    printf("Top of heap: %p\n", sbrk(0));
+static data_block* heap_liber[SIZE_HEAP];
+static data_block* start_node=NULL;
 
-    int* a= (int*)mmap(sbrk(0), sizeof(int), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    //int *addr=(int*)sbrk(sizeof(int));
-    *a = 12;
-    printf("nr dupa alocare: %i\n", *a);
-    printf("Adr a= %p\n",a);
+data_block* create_node(size_t dim){
+    data_block* new_node=(data_block*)sbrk(sizeof(data_block));
 
-    printf("Top of heap (după mmap): %p\n", sbrk(0));
-    int err=11;
-    err=munmap(a, sizeof(int));
+    new_node->dim_bloc=dim;
+    new_node->liber=false;
+    new_node->next=NULL;
+    new_node->prev=NULL;
+
+    return new_node;
+}
+
+void insert_new_node(data_block** head, size_t dim){
+    //data_block* curent_node=*head;
+    data_block* new_node=create_node(dim);
+
+    if(*head != NULL){
+        (*head)->next=new_node;
+    }
+
+    new_node->prev=*head;
+
+    *head=new_node;
+}
+
+void* mymalloc(size_t dim){
+    insert_new_node(&start_node,dim);
+
+    return (void*)&start_node;
+}
+
+int main(){
+    int* a=(int*)mymalloc(sizeof(int)*4);
+    for(int i=0;i<4;i++)
+    {
+        a[i]=i+3;
+    }
+
+    for(int i=0;i<4;i++){
+        printf("a[%i]= %i\n",i,a[i]);
+    }
     
-    sbrk(-1*sizeof(int));
-    printf("Adr a= %p\n",a);
-    // int a=*addr;
-    // printf("nr dupa dealocare: %i\n", a);
-
-    printf("Top of heap (după munmap): %p\n", sbrk(0));
     return 0;
 }
--- mmap_test.c
+++ mmap_test.c
@@ -5,19 +5,19 @@
 int main() {
     void* start_heap = sbrk(0);
     printf("Top of heap= %p\n",start_heap);
-    // Alocăm memorie folosind mmap în apropierea vârfului heap-ului
-    int* a = (int*)mmap(start_heap, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    
+    int* a = (int*)mmap(start_heap, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON | MAP_FIXED, -1, 0);
     if (a == MAP_FAILED) {
         perror("mmap");
         return 1;
     }
 
-    // Utilizăm memoria alocată cu mmap
+    
     *a=29;
     printf("Val lui a= %i\n",*a);
     start_heap=sbrk(0);
     printf("Top of heap= %p\n",start_heap);
-    // Eliberăm memoria utilizând munmap
+    
     if (munmap(a, 4096) == -1) {
         perror("munmap");
         return 1;
@@ -25,16 +25,23 @@ int main() {
 
     start_heap=sbrk(0);
 
-    int* b=(int*)mmap(start_heap, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-    *b=28;
+    int* b=(int*)mmap(start_heap, 4*sizeof(int), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    b[0]=1;
+    b[1]=2;
+    b[2]=3;
+    b[3]=4;
+    b[4]=5;
     //printf("Val lui b= %i\n",*b);
+
+    start_heap=sbrk(0);
+    void* end_heap=sbrk(0);
 
     if (munmap(b, 4096) == -1) {
         perror("munmap");
         return 1;
     }
 
-    start_heap=sbrk(0);
+    
 
     return 0;
 }
